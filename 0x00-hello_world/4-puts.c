877c26ce1cd640eb2b495430494c00f449490cde
azeez-abp <adioadeyoriazeez@gmail.com>
2023-07-13 06:39:27 -0700

    4 modify

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main(void)
+ /**
+  * main function 
+  * @params void
+  * @return int 0i
+  *
+  */
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return(0);
 }
