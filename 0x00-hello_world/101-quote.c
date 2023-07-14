013997e4acb019cf1075a217a7d735783c41fca7
azeez-abp <adioadeyoriazeez@gmail.com>
2023-07-14 01:56:29 -0700

    quote modify

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
- /**
- * C programme to print to std err 
+/**
  * main - Entry point
+ *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
     fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
     return (1);
-
 }
+
