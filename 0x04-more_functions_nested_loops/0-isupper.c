208f40034d29afc22f1c67fe12d594a2af9c7883
azeez-abp <adioadeyoriazeez@gmail.com>
2023-07-20 08:49:27 -0700

    upper case test done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isupper - check is uppervase
+ * @c:the char to check
+ * Description: chec if char is upper
+ * Return: 1 is uppercase
+ */
+
+int _isupper(int c)
+{
+	char alphabet;
+
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		if (c == alphabet)
+		}
+			return (1);
+		}
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+more loop class staryed
