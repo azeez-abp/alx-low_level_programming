fa20c82a65679992a01948926033bda580ce6bf3
azeez-abp <adioadeyoriazeez@gmail.com>
2023-07-21 01:29:36 -0700

    change

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 to 9
+ * Descroption: none
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		_putchar(num + '0')'
+	}
+
+	_puchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,6 @@
 #include <unistd.h>
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+
