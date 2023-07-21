25b326998951799bf57e5097d65f269859ea5798
azeez-abp <adioadeyoriazeez@gmail.com>
2023-07-21 02:54:49 -0700

    Added: #include main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - prints 1-14 10 times
+ *
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+        int limit = 10;
+        int num;
+
+        while (limit++)
+        {
+                for (num = 0; num <= 14; num++)
+                {
+                        int tens, ones;
+
+                        tens = num / 10;
+                        ones = num % 10;
+
+                        if (tens != 0)
+                                _putchar(tens + '0');
+
+                        _putchar(ones + '0');
+                }
+                _putchar('\n');
+        }
+}
