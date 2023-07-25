9bd4634b24c2754e7e2edac18422b6db6194628d
azeez-abp <adioadeyoriazeez@gmail.com>
2023-07-25 04:05:25 -0700

    100-change

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,26 +11,22 @@ int _atoi(char *s)
 	int sign  = 1;
 	int result = 0;
 
-	while (*s == ' ')
-		s++;
-	while (*s == '-' || *s == '+')
+	while (*s != '\0')
 	{
-		if(*s == '-')
+		 if(*s == '-')
 			sign =  -sign;
-		s++;
-	}
 
-	while (*s >= '0'  && *s <= '9')
-	{
-		int max  =  INT_MAX - (*s - '0');
-		int max_int = (int) (max / 10);
-
-		if (result > max_int)
+		if(*s >= '0' && *s <= '9')
 		{
-			return ((sign == 1) ? INT_MAX : INT_MIN);
-		}
+			int max  =  INT_MAX - (*s - '0');
+			int max_int = (int) (max / 10)
 
-		result = result * 10 + (*s - '0');
+			if (result > max_int)
+			{
+				return ((sign == 1) ? INT_MAX : INT_MIN);
+			}
+			result = (result * 10) + (*s - '0');
+		}
 		s++;
 	}
 
