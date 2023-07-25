5f7483c0e32400e25d44f53d57154bcf57fa30fa
azeez-abp <adioadeyoriazeez@gmail.com>
2023-07-25 04:32:23 -0700

    second 100 change

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,38 +1,36 @@
 #include <stdio.h>
 #include <limits.h>
-/**
- * _atoi - extract int in stirng.
- * @s: string to extract
- * Return: Alway success
- */
 
+/**
+ * _atoi - Extracts an integer from a string.
+ * @s: The string to extract from.
+ *
+ * Return: The extracted integer.
+ */
 int _atoi(char *s)
 {
-	int sign  = 1;
+	int sign = 1;
 	int result = 0;
 
 	while (*s != '\0')
 	{
-		 if (*s == '-')
-			sign =  -sign;
+		if (*s == '-')
+			sign = -sign;
 
 		if (*s >= '0' && *s <= '9')
 		{
-			int max  =  INT_MAX - (*s - '0');
+			int max = INT_MAX - (*s - '0');
 			int max_int = (int) (max / 10);
 
 			if (result > max_int)
 			{
 				return ((sign == 1) ? INT_MAX : INT_MIN);
 			}
-			
-			if ((*s++ >= 'A' && *s++ <= 'Z') || (*s++ >= 'a' && *s++ >= 'z') )
-				break;
+
 			result = (result * 10) + (*s - '0');
 		}
 		s++;
 	}
 
 	return (result * sign);
-
 }
