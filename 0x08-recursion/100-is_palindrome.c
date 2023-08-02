80cfdc431a50d7808b9396988548aa63b7ec9d9f
azeez-abp <adioadeyoriazeez@gmail.com>
2023-08-02 04:29:00 -0700

    One advance done

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,38 @@
+#include "main.c"
+#include <sting.h>
+/**
+ * palindrome_worker - check is palindrom
+ * @s:string input
+ * @i: the beging of string
+ * @j: the end of sting - null
+ * Return: 1 is palindrome, 0 is not palindrome
+ */
+int palindrome_worker(char *s, int i, int j)
+{
+	if (*s == 0)
+		return (1);
+
+	if (*s == '\0')
+		return (1);
+
+	if (s[i] != s[j])
+		return (0);
+
+	palindrome_worker(*s, i + 1, j - 1);
+}
+
+/**
+ * is_palindrome - check if string is palindrome
+ * @s: string input
+ * Return: 1 is palindrome, 0 is not
+ */
+int is_palindrome(char *s)
+{
+	int i = 0;
+	int j  = strlen(s);
+
+	if (palindrome_worker(*s, i, j) == 1)
+		return (1);
+	else
+		return (0);
+}
