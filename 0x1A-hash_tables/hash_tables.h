b3557a1496ae31dcb1782a2aa54e5b14c0c98bea
azeez-abp <adioazeezadeyori@gmail.com>
2023-12-21 17:33:57 -0800

    last print

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -34,6 +34,8 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+
+
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -41,5 +43,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+shash_table_t *shash_table_create(unsigned long int size);
+shash_node_t *set_spair(const char *key, const char *value);
+shash_node_t *set_spair_only(shash_table_t *ht, const char *key,
+			     const char *value, unsigned long int index);
+int update_value(shash_node_t *node, const char *value);
+shash_node_t *set_spair_front(shash_table_t *ht, const char *key,
+			     const char *value, unsigned long int index);
+int slist_set_first(shash_table_t *ht, shash_node_t *node);
+int slist_set(shash_table_t *ht, shash_node_t *old_node, shash_node_t *node);
+int slist_set_end(shash_table_t *ht, shash_node_t *node);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
 
 #endif
