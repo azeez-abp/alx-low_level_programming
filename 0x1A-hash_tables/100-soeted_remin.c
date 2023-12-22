d4f5c629e364984e32c099bb254eb69f5410343d
azeez-abp <adioazeezadeyori@gmail.com>
2023-12-21 17:49:34 -0800

    junck the 100

---
+++ 0x1A-hash_tables/100-soeted_remin.c
@@ -0,0 +1,103 @@
+#include "hash_tables.h"
+
+/**
+ * shash_table_get - retrieves a value associated with a key.
+ * @ht: a pointer to the sorted hash table array.
+ * @key: the key, a string that cannot be empty.
+ *
+ * Return: the value associated with the key, or NULL if key can't be found.
+ */
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	shash_node_t *node;
+
+	if (ht == NULL)
+		return (NULL);
+	if (key == NULL)
+		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
+		return (NULL);
+	if (strcmp(ht->array[index]->key, key) == 0)
+		return (ht->array[index]->value);
+	node = ht->array[index];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * shash_table_print - prints key/value pairs in sorted hash table order.
+ * @ht: the sorted hash table.
+ */
+void shash_table_print(const shash_table_t *ht)
+{
+	unsigned long int count = 0;
+	shash_node_t *node = ht->shead;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (node != NULL)
+	{
+		if (count > 0)
+			printf(", ");
+		printf("'%s': '%s'", node->key, node->value);
+		node = node->snext;
+		count++;
+	}
+	printf("}\n");
+}
+
+/**
+ * shash_table_print_rev - prints sorted key/value pairs in reverse.
+ * @ht: the sorted hash table.
+ */
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	unsigned long int count = 0;
+	shash_node_t *node = ht->stail;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (node != NULL)
+	{
+		if (count > 0)
+			printf(", ");
+		printf("'%s': '%s'", node->key, node->value);
+		node = node->sprev;
+		count++;
+	}
+	printf("}\n");
+}
+
+/**
+ * shash_table_delete - deletes a sorted hash table
+ * @ht: the sorted hash table.
+ */
+void shash_table_delete(shash_table_t *ht)
+{
+	unsigned long int i = 0;
+	shash_node_t *node;
+
+	while (i < ht->size)
+	{
+		while (ht->array[i] != NULL)
+		{
+			node = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = node;
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
+}
