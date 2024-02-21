1eecb22ee65505cc62917ffac1d663ca2ba3f5e3
azeez-abp <adioazeezadeyori@gmail.com>
2024-02-21 13:47:36 -0800

    05-amend

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
+"""5-island_perimeter module"""
 
-"""Module for calculating the perimeter of island"""
 
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+    c = 0
+    length = len(grid) - 1
+    width = len(grid[0]) - 1
 
-def island_perimeter(grid: list) -> int:
-    """get the number of water zone if there is
-       only one land zone in the same row
-       Args:
-       grid (list): A 2dD representation
-       Return:
-       int : represent the perimeter
-    """
-    count = 0
-    for index_x, x in enumerate(grid):
-        start = e
-        point_of_1 = 0
-        point_of_1_count = 0
-        encounter = False
-        while start < len(x):
-            if x[start] == 1:
-                encounter = True
-                point_of_1 = start
-                point_of_1_count += 1
-
-            if encounter is True and x[start] == 0:
-                count += 1
-            start += 1
-        """count the water zone behind"""
-
-        if point_of_1 > 0 and point_of_1_count == 1:
-            while point_of_1 >= 0:
-                if x[point_of_1] == 0:
-                    count += 1
-                point_of_1 -= 1
-    return count
+    for i, r in enumerate(grid):
+        for j, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][j] != 1:
+                    c += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    c += 1
+                if j == width or grid[i][j + 1] != 1:
+                    c += 1
+                if i == length or grid[i + 1][j] != 1:
+                    c += 1
+    return c
